c41e16ceddc8bd417d0e0d295a4d548b370c6199
Matthias Puech <puech@cs.mcgill.ca>
2015-05-06 00:23:28 -0400

    Clouds: looper/shifter: lower Q of LP filter.
    Just by taste

--- clouds/dsp/granular_processor.cc
+++ clouds/dsp/granular_processor.cc
@@ -312,6 +312,7 @@ void GranularProcessor::Process(
     diffuser_.set_amount(diffusion);
     diffuser_.Process(out_, size);
   }
+
 #define LIMIT 0.1f
 #define SLEW 0.02f
   
@@ -340,7 +341,7 @@ void GranularProcessor::Process(
         (cutoff < 0.5f ? -0.5f : cutoff - 1.0f) * 216.0f);
     CONSTRAIN(lp_cutoff, 0.0f, 0.499f);
     CONSTRAIN(hp_cutoff, 0.0f, 0.499f);
-    float lpq = 1.0f + 3.0f * (1.0f - feedback) * (0.5f - lp_cutoff);
+    float lpq = 1.0f + 2.0f * (1.0f - feedback) * (0.5f - lp_cutoff);
     lp_filter_[0].set_f_q<FREQUENCY_FAST>(lp_cutoff, lpq);
     lp_filter_[0].Process<FILTER_MODE_LOW_PASS>(
         &out_[0].l, &out_[0].l, size, 2);
